5026757d5a20df7dad202737e940d37b24fa8052
Ewann Postic <63552544+Epwo@users.noreply.github.com>
2025-02-11 15:30:49 +0100

    :flame: Working v0 (need review)

--- Classes/FourmiClass.py
+++ Classes/FourmiClass.py
@@ -4,14 +4,14 @@ from Classes.LieuClass import Lieu
 class Fourmi:
     def __init__(self, start_point: Lieu, nb):
         self.start_point = start_point
-        self.current_point = start_point
         self.visited_points = [start_point]
+        self.current_point = start_point
         self.nom = "micheline " + str(nb)
 
     def get_attributes(self):
         return f"""
         Fourmi {self.nom}
         Start point: {self.start_point.x, self.start_point.y}, Point n°{self.start_point.nom}
-        Current point: {self.start_point.x, self.start_point.y}, Point n°{self.start_point.nom}
+        Current point: {self.current_point.x, self.current_point.y}, Point n°{self.current_point.nom}
         Visited points: {[(point.x, point.y,point.nom) for point in self.visited_points]}
         """
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from logic import TSP_ACO
 
-nb_fourmis = 5
-nb_iter = 1
+nb_fourmis = 3
+nb_iter = 5
 alpha = 1
 beta = 1
 rho = 0.5
@@ -12,15 +12,16 @@ list_lieux = []
 with open("src/graph_5.csv", "r") as f:
     next(f)  # Skip header line
     for i, line in enumerate(f):
-        x = float(line.split(",")[1])
+        x = float(line.split(",")[0])
         y = float(line.split(",")[-1].strip())
         list_lieux.append([x, y, i])
 
 aco = TSP_ACO(list_lieux, nb_fourmis, nb_iter, alpha, beta, rho, q, q0)
 aco.init_fourmis()
 
+
+aco.run()
+
 # for e in aco.fourmis:
 #     print(e.get_attributes())
 # print("coo")
-
-aco.wip()
