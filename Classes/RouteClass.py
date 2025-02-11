f51aa32cf39f8278fea969bbd6aea96c59eaa8fa
Ewann Postic <63552544+Epwo@users.noreply.github.com>
2025-02-11 15:31:42 +0100

    :fire: Working v0 (need review)

--- Classes/RouteClass.py
+++ Classes/RouteClass.py
@@ -1,15 +1,17 @@
+from Classes.LieuClass import Lieu
+
+
 class Route:
-    def __init__(self, ordre, matrice_od):
+    def __init__(self, ordre: list[Lieu], matrice_od):
         self.ordre = ordre
         self.matrice_od = matrice_od
 
     def calcul_distance_route(self):
-
         distance_totale = 0.0
 
         for i in range(len(self.ordre) - 1):
             lieu_depart = self.ordre[i]
             lieu_arrivee = self.ordre[i + 1]
-            distance_totale += self.matrice_od[lieu_depart][lieu_arrivee]
+            distance_totale += self.matrice_od[lieu_depart.nom][lieu_arrivee.nom]
 
         return distance_totale
--- logic.py
+++ logic.py
@@ -20,21 +20,27 @@ class TSP_ACO(Graph):
         self.graph.calcul_matrice_cout_od()
 
     def init_fourmis(self):
+        self.fourmis = []
         for i in range(self.nb_fourmis):
             # set the start point
             start_point = self.graph.liste_lieux[i % self.graph.nb_lieux]
             self.fourmis.append(Fourmi(start_point, i))
 
-    def wip(self):
+    def run(self):
         for i in range(self.nb_iter):
             # new fourmis generation
-            for fourmi in self.fourmis:
-                # for each fourmi, we calculate the next point
-                self.forward(
-                    fourmi,
-                    self.graph,
-                )
-            # self.update_pheromones()
+            print(f"--- Iteration {i} ---")
+            while len(self.fourmis[0].visited_points) <= self.graph.nb_lieux:
+                for fourmi in self.fourmis:
+                    # for each fourmi, we calculate the next point
+                    self.forward(
+                        fourmi,
+                        self.graph,
+                    )
+                # finish the trajet, before updating pheromones
+            print(f"finished  G{i} updating pheromones")
+            self.update_pheromones(fourmis=self.fourmis)
+            self.init_fourmis()
 
     def calc_next_point(
         self,
@@ -48,13 +54,16 @@ class TSP_ACO(Graph):
 
         for point in points:
             max_value = int(max(max(row) for row in mat_od))
+            print(f"current point {current_point} ; next point {point.nom}")
             is_unvisited = 1 if point not in visited_points else 0
+            print(f"isUnvisited {is_unvisited == 1}")
             eq = (
                 -(mat_od[current_point][point.nom] * (1 / 3))
                 + (mat_phero[current_point][point.nom] * (1 / 3))
                 + (1 / 3) * random.randint(0, max_value)
             ) * is_unvisited
             probas[point.nom] = eq
+        print(probas)
         return probas
 
     def forward(
@@ -71,17 +80,47 @@ class TSP_ACO(Graph):
             mat_phero=graph.matrice_pheromones,
         )
 
-        print(proba)
         # choose the next point
-        next_point_name = max(proba, key=proba.get)
+        proba = {k: v for k, v in proba.items() if v != 0}
+        # we remove all of the 0 values
+        if proba == {}:
+            # we are on the last point, lets go back on the first one
+            next_point_name = graph.liste_lieux[0].nom
+        else:
+            next_point_name = max(proba, key=proba.get)
+            print(">>", next_point_name)
+
         next_point = next(
             point for point in graph.liste_lieux if point.nom == next_point_name
         )
         print(
-            f"Next point: {next_point.nom}, {next_point.x, next_point.y} for dist {proba[next_point_name]}"
+            f"  for {fourmi.nom} Next point: {next_point.nom}, {next_point.x, next_point.y}"
         )
-        # next_point = fourmi.choose_next_point(proba, q, q0)
-        # # update the current point
-        # self.current_point = next_point
-        # # add the point to the visited points
-        # fourmi.visited_points.append(next_point)
+        fourmi.current_point = next_point
+        # add the point to the visited points
+        fourmi.visited_points.append(next_point)
+        print(fourmi.get_attributes())
+
+    def update_pheromones(self, fourmis: list[Fourmi]):
+        dist_routes = {}
+        for i, fourmi in enumerate(fourmis):
+            list_lieux = fourmi.visited_points
+            route = Route(list_lieux, self.graph.matrice_cout_od)
+            dist_routes[i] = route.calcul_distance_route()
+        best_dist = min(dist_routes.values())
+        best_route: list[Lieu] = fourmis[
+            min(dist_routes, key=dist_routes.get)
+        ].visited_points
+        print(
+            f"best route: {best_route} for fourmi {min(dist_routes, key=dist_routes.get)} for dist {best_dist}"
+        )
+        # update pheromones
+        print(f"olds pheromones: {self.graph.matrice_pheromones}")
+        for i in range(len(best_route) - 1):
+            x_value = best_route[i].nom
+            y_value = best_route[i + 1].nom
+            self.graph.matrice_pheromones[x_value][y_value] = (
+                (max(max(row) for row in self.graph.matrice_cout_od)) / 2
+            ) + self.graph.matrice_pheromones[x_value][y_value]
+
+        print(f"new pheromones: \n {self.graph.matrice_pheromones}")
